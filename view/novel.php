311e4a33193c705128a85a9b2f7239282e63cfcf
alafrance <alexislafrances-laf@outlook.fr>
2020-03-15 15:49:53 +0100

    fix novel

--- view/novel.php
+++ view/novel.php
@@ -1,12 +1,14 @@
 <?php
 $this->title = 'Billet simple pour l\'alaska';
+$i = 0;
 ?>
 <?php
 foreach ($articles as $article)
 {
+    $i += 1;
     ?>
     <article>
-        <h2><?= htmlspecialchars($article->getTitle());?></h2>
+        <h2>Chapitre <?= $i ?> : <?= htmlspecialchars($article->getTitle());?></h2>
         <div>
                 <?= substr($article->getContent(), 0, 600);?>...<br>
                 <a href="../public/index.php?route=article&id=<?= htmlspecialchars($article->getId());?>">Lire la suite</a>
